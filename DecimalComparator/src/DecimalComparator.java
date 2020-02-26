7922ffae5072a9641fede7b59347325eecae9a76
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-02-26 14:30:58 -0500

    Update DecimalComparator.java

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -2,6 +2,8 @@ public class DecimalComparator {
 
     public static boolean areEqualByThreeDecimalPlaces(double paper, double pencil){
 
+        
+        //variables
         paper = paper * 1000;
         pencil = pencil * 1000;
 
@@ -9,6 +11,7 @@ public class DecimalComparator {
         int paper1 = (int) paper;
         int pencil2 = (int) pencil;
 
+        //if-loop
 
         if(paper1==pencil2){
             return true;
