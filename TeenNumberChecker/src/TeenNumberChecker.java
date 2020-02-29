55e3339a29ec80438a95c8f6c8df2a6434fc4ba7
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-02-29 15:12:01 -0500

    Update TeenNumberChecker.java

--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -2,6 +2,8 @@ public class TeenNumberChecker {
 
     public static boolean hasTeen(int a, int b, int c){
 
+        //if a/b/c is teen
+        //if-loop
     if((a>13 || a<19)|| (b>13 || a<19) || (c>13 || c<19)){
         return true;
     }else{
@@ -12,7 +14,7 @@ public class TeenNumberChecker {
 
     }
     public static boolean isTeen(int a){
-
+//if is teen return true else if not return false
         if(a>=13 || a<=19){
             return true;
         }else{
