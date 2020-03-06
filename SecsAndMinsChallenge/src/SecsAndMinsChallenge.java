0cf7cbbfb5112e73de9442018f7b97d08cd35090
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-03-06 16:44:40 -0500

    Update SecsAndMinsChallenge.java

--- SecsAndMinsChallenge/src/SecsAndMinsChallenge.java
+++ SecsAndMinsChallenge/src/SecsAndMinsChallenge.java
@@ -1,30 +1,46 @@
 public class SecsAndMinsChallenge {
 
-    public static void getDurationString(int minutes, int seconds){
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
 
-        int hours;
-
-        if((minutes>=0)&&(seconds>=0 && seconds<=59)){
-            hours = minutes / 60;
-            minutes = minutes % 60;
-            System.out.println(hours +"h " + minutes +"m " + seconds+"s");
-        }else{
-            System.out.println("Invalid Value");
-        }
-
-
-    }
     public static void main(String[] args){
-        getDurationString(156,50);
-
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945L));
     }
 
-    public static void getDurationString(int seconds){
+    private static String getDurationString(long minutes, long seconds){
+        if((minutes<0) || (seconds<0) || (seconds>59)){
 
-        if(seconds>=0){
-            
+            return INVALID_VALUE_MESSAGE;
         }
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+
+        String hoursString = hours +"h";
+        if(hours< 10){
+            hoursString = "0" + hoursString;
+        }
+        String minutesString = remainingMinutes +"h";
+        if(remainingMinutes< 10){
+            minutesString = "0" + minutesString;
+        }
+        String secondsString = seconds +"h";
+        if(seconds< 10){
+            secondsString = "0" + secondsString;
+        }
+        return hoursString +"  "+minutesString + " " + seconds +"";
     }
 
+
+    private static String getDurationString(long seconds){
+
+        if(seconds<0){
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+        return getDurationString(minutes, remainingSeconds);
+
+    }
 
 }
